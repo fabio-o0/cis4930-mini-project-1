eae442be28b3f8b3a41b75b50be9e1cadbac8c09
Fabio Suarez <fabiosuarez.o0@gmail.com>
2020-03-13 12:29:19 -0400

    .

--- vulnerable_app/main.py
+++ vulnerable_app/main.py
@@ -30,6 +30,16 @@ db = sqlalchemy.create_engine(
     pool_recycle=1800,  # 30 minutes
 )
 
+def getUsers():
+    users = []
+    with db.connect() as conn:
+        all_users = conn.execute(
+            "SELECT username, password FROM users"
+        ).fetchall()
+        for row in all_users:
+            users.append({"username": row[0], "password": row[1]})
+    return users
+
 @app.before_first_request
 def create_tables():
     # Create tables (if they don't already exist)
@@ -40,20 +50,56 @@ def create_tables():
             "password VARCHAR(50) NOT NULL, PRIMARY KEY (username) );"
         )
 
-@app.route("/", methods=["GET"])
-def index():
-    users = []
-    with db.connect() as conn:
-        # Execute the query and fetch all results
-        all_users = conn.execute(
-            "SELECT username, password FROM users"
-        ).fetchall()
-        # Convert the results into a list of dicts representing votes
-        for row in all_users:
-            users.append({"username": row[0], "password": row[1]})
+# @app.route("/", methods=["GET", "POST"])
+# def index():
+#     message = ""
+#     if request.method == "POST":
+#         user = request.form.get('username')
+#         passw = request.form.get('password')
+#
+#         if user = 'root' and passw = '1234':
+#             message = "user found"
+#         else:
+#             message = "user not found"
+#
+#     return render_template("index.html", message = message)
+
+@app.route('/login/', methods=['post', 'get'])
+def login():
+    users = getUsers()
+    message = ''
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        find = {"username": username, "password": password}
+        if find in users:
+            message = "Correct username and password"
+        else:
+            message = "Wrong username or password"
+
+    return render_template('index.html', message=message)
+
+# @app.route('/create_account/', methods=['post', 'get'])
+# def create_account():
+#     if request.method = 'POST':
+#         new_username = request.form.get('new_username')
+#         new_password = request.form.get('new_username')
+#
+#         with db.connect() as conn:
+#             conn.execute(
+#                 f"INSERT INTO users VALUES ({new_username}, {new_password});"
+#             )
+#
+#         return render_template("users.html", all_users=users, tab_count=0, space_count=0)
+#     return render_template('create.html')
+
+@app.route("/users/", methods=["GET"])
+def users():
+    users = getUsers()
 
     return render_template(
-        "index.html", all_users=users, tab_count=0, space_count=0
+        "users.html", all_users=users, tab_count=0, space_count=0
     )
 
 if __name__ == "__main__":
